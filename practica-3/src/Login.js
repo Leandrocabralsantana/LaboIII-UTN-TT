afa957606b143798e9ce686b2ac182a295b9a10d
Leandro Cabral Santana <100740901+Leandrocabralsantana@users.noreply.github.com>
2023-10-22 19:59:39 -0300

    Practica 3
    Practica 3

---
+++ practica-3/src/Login.js
@@ -0,0 +1,40 @@
+import { useState } from "react";
+
+export const Login = () => {
+  const [username, setUsername] = useState("");
+
+  const handleLogin = (username) => {
+    alert(
+      `Usuario ingresado exitosamente. Su nombre de usuario es: ${username}`
+    );
+  };
+
+  const handleClick = () => {
+    if (username.includes("o" || "O")) {
+      alert("Usuario inválido para registrarse");
+    } else {
+      handleLogin(username);
+    }
+  };
+
+  const handleUsername = (e) => {
+    setUsername(e.target.value);
+    if (e.target.value.includes("o" || "O")) {
+      alert("Por favor, ¡Nombres de usuario sin la letra o!");
+    }
+  };
+  return (
+    <>
+      <label>Ingrese su nombre de usuario</label>
+      <input id={username} onChange={handleUsername} />
+      <button
+        onClick={() => {
+          handleClick();
+        }}
+      >
+        Registrarse
+      </button>
+      <p>El nombre de usuario con el que se va a registrar es: {username}</p>
+    </>
+  );
+};
